3f92f7c28d1c10438140df9ee4b42bc2789a8960
Nicholas Picini <npicini@optonline.net>
2022-09-05 02:43:02 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('ignore')
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
         if logs.get('accuracy') is not None and logs.get('accuracy') > 0.995:
-            print("\nReached 99.9% accuracy so cancelling training!")
+            print("\nReached 99.5% accuracy: cancelling training!")
             self.model.stop_training = True
 
 
