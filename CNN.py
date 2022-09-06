480f0683ca95d1d945fb839215f411546402a314
Nicholas Picini <npicini@optonline.net>
2022-09-06 15:11:45 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -116,7 +116,7 @@ plt.show()
 import numpy as np
 from keras.preprocessing import image
 
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64, 64))
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size=(150, 150))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 result = cnn.predict(test_image)
